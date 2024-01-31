4f67cd6f5de6c5e7840ce5f7d21682ff3a6d4f8a
nam <npnam031101@gmail.com>
2024-01-31 12:15:24 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import { useEffect, useRef } from 'react';
-import { useCodeMirror } from '@uiw/react-codemirror';
+// #Link Theme:  https://uiwjs.github.io/react-codemirror/
+
+import { darcula } from '@uiw/codemirror-theme-darcula';
+import * as events from '@uiw/codemirror-extensions-events';
+import { sublime } from '@uiw/codemirror-theme-sublime';
 import { javascript } from '@codemirror/lang-javascript';
+import CodeMirror from '@uiw/react-codemirror';
+import { EditorView } from "@codemirror/view"
+import { useState } from 'react';
 
-const code = "console.log('hello world!');\n\n\n";
-const extensions = [javascript()];
+import { color } from '@uiw/codemirror-extensions-color';
 
-export default function App() {
-  const editor = useRef();
-  const { setContainer } = useCodeMirror({
-    container: editor.current,
-    extensions,
-    value: code,
+function App() {
+  const eventExt = events.content({
+    focus: (evn) => {
+      console.log('focus');
+    },
+    blur: (evn) => {
+      console.log('blur');
+    },
+    keydown: (evn) => {
+      if (evn.ctrlKey && evn.code === 'KeyS') {
+        console.log('Saved!');
+        evn.preventDefault();
+      }
+    },
   });
 
-  useEffect(() => {
-    if (editor.current) {
-      setContainer(editor.current);
-    }
-  }, [editor.current]);
-
-  return <div ref={editor} />;
+  return (
+    <CodeMirror
+      value="body { color: #333333; }"
+      height="100vh"
+      width='100%'
+      style={{fontSize: '14px'}}
+      theme={darcula}
+      extensions={[color, javascript({ jsx: true }),EditorView.lineWrapping, eventExt]}
+      onChange={(value, viewUpdate) => { console.log('value:', value);}}
+    />
+  );
 }
+export default App;
