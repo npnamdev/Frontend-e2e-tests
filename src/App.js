5dd6a7f3738726c6d8b49163d39cc95fca10a1fc
Nguyễn Phương Nam <119719970+npnamdev@users.noreply.github.com>
2024-01-31 16:46:41 +0700

    update code them mới

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
-import React from 'react';
-import MonacoEditor from 'react-monaco-editor';
+import { useEffect, useRef } from 'react';
+import { useCodeMirror } from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
 
-const App = () => {
-  const code = `const helloWorld = () => {
-    console.log('Hello, World!');
-  };`;
+const code = "console.log('hello world!');\n\n\n";
+const extensions = [javascript()];
 
-  const handleEditorChange = (newValue, e) => {
-    // Xử lý sự kiện thay đổi nếu cần thiết
-    console.log('onChange', newValue, e);
-  };
+export default function App() {
+  const editor = useRef();
+  const { setContainer } = useCodeMirror({
+    container: editor.current,
+    extensions,
+    value: code,
+  });
 
-  return (
-    <div>
-      <h1>React App</h1>
-      <MonacoEditor
-        width="800"
-        height="600"
-        language="javascript"
-        theme="vs-dark" // Chọn theme theo ý muốn
-        value={code}
-        options={{
-          selectOnLineNumbers: true,
-          roundedSelection: false,
-          cursorStyle: 'line',
-          automaticLayout: true,
-        }}
-        onChange={handleEditorChange}
-      />
-    </div>
-  );
-};
+  useEffect(() => {
+    if (editor.current) {
+      setContainer(editor.current);
+    }
+  }, [editor.current]);
 
-export default App;
+  return <div ref={editor} />;
+}
