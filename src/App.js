cf78397de7223215fd50ade9351ef7565799922e
nam <npnam031101@gmail.com>
2024-03-21 21:17:12 -0500

    update socketio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import CodeEditor from './components/CodeEditor';
-import TreeExample from './components/TreeExample';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:4000');
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    socket.on('updateInput', (data) => {
+      setInputValue(data); // Cập nhật giá trị input từ server
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const handleInputChange = (event) => {
+    const newValue = event.target.value; // Lưu trữ giá trị mới từ sự kiện thay đổi
+    setInputValue(newValue); // Cập nhật giá trị input với giá trị mới
+  
+    // Gửi giá trị input mới lên server
+    socket.emit('updateInput', newValue); 
+  };
+  
+
   return (
-    <div className='app-container'>
-      <TreeExample />
-      <CodeEditor />
+    <div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Type something..."
+      />
     </div>
   );
 }
+
 export default App;
